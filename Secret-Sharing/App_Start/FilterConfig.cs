0427fc02b8447c800674501b5883678f1f8696c5
Quốc Anh <20127001@student.hcmus.edu.vn>
2023-05-24 01:47:17 +0700

    Create Project

---
+++ Secret-Sharing/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Secret_Sharing
+{
+	public class FilterConfig
+	{
+		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+		{
+			filters.Add(new HandleErrorAttribute());
+		}
+	}
+}
---
+++ Secret-Sharing/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Secret_Sharing.MvcApplication" Language="C#" %>
